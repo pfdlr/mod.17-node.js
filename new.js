0fc0fd22a7ba116e0d2b44414c37fd2146ee31e9
Piotr Fidler <fidler.piotr@gmail.com>
2019-07-23 20:02:39 +0200

    first commit task 17.3

---
+++ new.js
@@ -0,0 +1,28 @@
+process.stdin.setEncoding("utf-8");
+var lang = process.env.LANG;
+var ver = process.versions.node;
+var release = process.release.lts;
+process.stdout.write("Possibilities: /ver, /lang, /release, /exit\n");
+
+process.stdin.on("readable", function() {
+  var input = process.stdin.read();
+  if (input !== null) {
+    var instruction = input.toString().trim();
+    switch (instruction) {
+      case "/exit":
+        process.stdout.write("Quitting app!\n");
+        process.exit();
+      case "/ver":
+        process.stdout.write("NODE version: " + ver + "\n");
+        break;
+      case "/lang":
+        process.stdout.write("System language: " + lang + "\n");
+        break;
+      case "/release":
+        process.stdout.write("NOD release: " + release + "\n");
+        break;
+      default:
+        process.stdout.write("Wrong instruction!\n");
+    }
+  }
+});
